c343a86b8ff75191730067494d8fd2df87d71b65
vlecsei <vlecsei@gmail.com>
2018-08-20 14:15:15 +0200

    Initial commit

---
+++ src/com/vladimir/application/Principale.java
@@ -0,0 +1,15 @@
+package com.vladimir.application;
+
+public class Principale {
+
+    public static void main(String[] args) {
+	
+	
+	System.out.println("-------------------------------------------------------------");
+	System.out.println("Hello World !");
+	System.out.println("Mon premier programme Java fonctionne !");
+	System.out.println("-------------------------------------------------------------");
+
+    }
+
+}
